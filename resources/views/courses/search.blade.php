34b936b9d1b5cc57455f3d8297d466046da24395
themvlek <malik777khaled@gmail.com>
2026-08-05 04:23:50 +0200

    Fix top padding added at larger screen

--- resources/views/courses/search.blade.php
+++ resources/views/courses/search.blade.php
@@ -5,7 +5,8 @@
 @section('tg-root', 'true')
 
 @section('content')
-    <div class="flex flex-col items-center min-h-screen px-0 md:px-6 pb-0 md:pb-6 pt-0 md:pt-[clamp(0rem,calc((100vw-48rem)*0.094),1.5rem)]">
+    {{-- <div class="flex flex-col items-center min-h-screen px-0 md:px-6 pb-0 md:pb-6 pt-0 md:pt-[clamp(0rem,calc((100vw-48rem)*0.094),1.5rem)]"> --}}
+    <div class="flex flex-col items-center px-0 md:px-6 pb-0 md:pb-6 pt-0">
         <div class="course-flow-page flex flex-col min-h-screen md:min-h-0" style="overflow: visible;">
 
             <div class="pt-10 pb-6 px-6">
